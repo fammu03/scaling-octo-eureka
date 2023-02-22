c9a5ebf64e6a8470becf7fb101da1a81b662798a
Faim <77967904+fammu03@users.noreply.github.com>
2023-02-23 02:38:36 +0530

    added QR

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
 const container = document.querySelector(".container")
+
+function onScanSuccess(decodedText, decodedResult) {
+  // handle the scanned code as you like, for example:
+  console.log(`Code matched = ${decodedText}`, decodedResult);
+}
+
+function onScanFailure(error) {
+  // handle scan failure, usually better to ignore and keep scanning.
+  // for example:
+  console.warn(`Code scan error = ${error}`);
+}
+
+let html5QrcodeScanner = new Html5QrcodeScanner(
+  "reader",
+  { fps: 10, qrbox: {width: 250, height: 250} },
+  /* verbose= */ false);
+html5QrcodeScanner.render(onScanSuccess, onScanFailure);
